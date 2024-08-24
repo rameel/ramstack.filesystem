a11089edd43e3ee643f4145e9600f3e927d331f7
rameel <rameel-b@hotmail.com>
2024-08-25 02:18:42 +0500

    Add tests

---
+++ tests/Ramstack.FileSystem.Composite.Tests/CompositeFileSystemTests.cs
@@ -0,0 +1,45 @@
+﻿using Ramstack.FileSystem.Physical;
+using Ramstack.FileSystem.Prefixed;
+using Ramstack.FileSystem.Specification.Tests;
+using Ramstack.FileSystem.Specification.Tests.Utilities;
+
+namespace Ramstack.FileSystem.Composite;
+
+[TestFixture]
+public class CompositeFileSystemTests : VirtualFileSystemSpecificationTests
+{
+    private readonly TempFileStorage _storage = new TempFileStorage();
+    private readonly CompositeFileSystem _fs;
+
+    public CompositeFileSystemTests()
+    {
+        var root = Path.Join(_storage.Root, "project");
+
+        foreach (var file in Directory.GetFiles(root))
+            File.Delete(file);
+
+        var list = new List<IVirtualFileSystem>();
+        foreach (var directory in Directory.GetDirectories(root))
+        {
+            var fileName = Path.GetFileName(directory);
+            Console.WriteLine(fileName);
+
+            var fs = new PrefixedFileSystem(fileName, new PhysicalFileSystem(directory));
+            list.Add(fs);
+        }
+
+        _fs = new CompositeFileSystem(list);
+    }
+
+    [OneTimeTearDown]
+    public void Cleanup()
+    {
+        _fs.Dispose();
+    }
+
+    protected override IVirtualFileSystem GetFileSystem() =>
+        _fs;
+
+    protected override DirectoryInfo GetDirectoryInfo() =>
+        new DirectoryInfo(Path.Join(_storage.Root, "project"));
+}
