7cdb9f442a1cb5d73b595ee12f0acd6f96b83ffb
rameel <rameel-b@hotmail.com>
2025-08-11 01:23:36 +0500

    Throw UnauthorizedAccessException when deleting artificial read-only directories

--- tests/Ramstack.FileSystem.Prefixed.Tests/PrefixedFileSystemTests.cs
+++ tests/Ramstack.FileSystem.Prefixed.Tests/PrefixedFileSystemTests.cs
@@ -9,6 +9,54 @@ public class PrefixedFileSystemTests : VirtualFileSystemSpecificationTests
 {
     private readonly TempFileStorage _storage = new TempFileStorage();
 
+    [Test]
+    public async Task File_Create_InsideArtificialDirectory_ThrowsException()
+    {
+        using var fs = GetFileSystem();
+
+        await Assert.ThatAsync(
+            async () => await fs.WriteAsync("/ea5fd219.txt", Stream.Null),
+            Throws.Exception);
+
+        Assert.That(
+            await fs.FileExistsAsync("/ea5fd219.txt"),
+            Is.False);
+    }
+
+    [Test]
+    public async Task Directory_Delete_ArtificialDirectory_ThrowsException()
+    {
+        using var storage = new TempFileStorage();
+        using var fs = new PrefixedFileSystem("/bin/apps/myapp1", new PhysicalFileSystem(storage.Root));;
+
+        await Assert.ThatAsync(
+            async () => await fs.DeleteDirectoryAsync("/"),
+            Throws.Exception);
+
+        await Assert.ThatAsync(
+            async () => await fs.DeleteDirectoryAsync("/bin"),
+            Throws.Exception);
+
+        await Assert.ThatAsync(
+            async () => await fs.DeleteDirectoryAsync("/bin/apps"),
+            Throws.Exception);
+
+        Assert.That(
+            await fs.FileExistsAsync("/bin/apps/myapp1/project/README.md"),
+            Is.True);
+
+        await Assert.ThatAsync(
+            async () => await fs.DeleteDirectoryAsync("/bin/apps/myapp1"),
+            Throws.Nothing);
+
+        Assert.That(
+            await fs.FileExistsAsync("/bin/apps/myapp1/project/README.md"),
+            Is.False);
+    }
+
+    [OneTimeTearDown]
+    public void Cleanup() =>
+        _storage.Dispose();
 
     protected override IVirtualFileSystem GetFileSystem() =>
         new PrefixedFileSystem("/project",
